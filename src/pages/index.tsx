d98422cb26a401f29697599b152d2da23503d9be
Elton Andrade <eltonandradee@gmail.com>
2021-04-09 23:57:03 -0300

    add React Testing Library config and first test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,11 @@
-import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
+import Head from "next/head"
+import styles from "../../styles/Home.module.css"
 
 type Props = {
-  title: string;
-};
+  title: string
+}
 
-export default function Home
-
-
-
-({title = "React Avançado!"}: Props) {
+export default function Home({ title = "React Avançado!" }: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -23,7 +19,7 @@ export default function Home
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -64,7 +60,7 @@ export default function Home
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
